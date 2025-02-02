560d6c523ea9a434de8a2aac4009a19a6931b74f
Sagor-Ahammer <badshasagor123456789@gmail.com>
2025-02-02 19:16:00 +0600

    some code is changed

--- hello.java
+++ hello.java
@@ -1,10 +1,11 @@
 pablic class Hello {
     pablic static void  main(string[]arge)(
         system.out.println(" Vs code is awesome")
+        greeting ( "Git")
 
     )
     pablic static void  greeting (string name){
-        system.out.println ("wellcome" + name);
+        system.out.println ("Bye Bye" + name);
     
     }
 }
